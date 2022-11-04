58109e82a28b1dc81a8b82bec272ad0aa300d5c4
Amina Rahman Nisha <aminarahman01@gmail.com>
2022-11-04 16:57:52 +0600

    basic signin done

--- server.js
+++ server.js
@@ -2,12 +2,42 @@ const express = require('express');
 
 const app = express();
 
+//instead of body-parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com', 
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com', 
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
 app.get('/', (req, res) => {
 	res.send('This is working');
 })
 
 app.post('/signin', (req, res) => {
-	res.json('signing in');
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+		res.json('success');
+	} else {
+		res.status(400).json('error signing in');
+	}
 })
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
