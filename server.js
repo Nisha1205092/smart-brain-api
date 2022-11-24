bc34cc4a823620d6116a67cf10595b2b21500dfc
Amina Rahman Nisha <aminarahman01@gmail.com>
2022-11-24 23:03:17 +0600

    log hash of password

--- server.js
+++ server.js
@@ -24,10 +24,8 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-    	rejectUnauthorized: false
-    }
-  }
+    ssl: { rejectUnauthorized: false },
+  },
 });
 
 // db.select('*').from('users')
