a7097a2819e30e57ab4a824128b4c066fc37d661
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-04-19 19:56:39 +0600

    added SQL as comment for table creation

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,19 @@
+// CREATE TABLE users(
+// 	id serial PRIMARY KEY,
+// 	name VARCHAR(100),
+// 	email text UNIQUE NOT NULL,
+// 	entries INT DEFAULT 0,
+// 	joined TIMESTAMP NOT NULL
+// );
+
+// CREATE TABLE login(
+// 	id serial PRIMARY KEY,
+// 	email text UNIQUE NOT NULL,
+// 	hash text UNIQUE NOT NULL
+// );
+
 const handleRegister = (req, res, db, bcrypt) => {
-	const {email, name, password} = req.body;
+	const { email, name, password } = req.body;
 	if (!email || !name || !password) {
 		console.log(`name: ${name} email: ${email} password: ${password}`);
 		return res.status(400).json('empty field submission');
@@ -9,28 +23,28 @@ const handleRegister = (req, res, db, bcrypt) => {
 
 	db.transaction(trx => {
 		trx.insert({
-			hash: hash, 
+			hash: hash,
 			email: email
 		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-				.returning('*')
-				.insert({
-					email: loginEmail[0].email,
-					name: name, 
-					joined: new Date()
-				})
-				.then(user => {
-					res.json(user[0]);
-				})
-				
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+					.returning('*')
+					.insert({
+						email: loginEmail[0].email,
+						name: name,
+						joined: new Date()
+					})
+					.then(user => {
+						res.json(user[0]);
+					})
+
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
 	})
-	.catch(err => res.status(400).json('unable to register'));
+		.catch(err => res.status(400).json('unable to register'));
 	// res.json(database.users[database.users.length - 1]);
 }
 module.exports = {
--- server.js
+++ server.js
@@ -13,14 +13,14 @@ const PORT = process.env.PORT || 3031;
 const db = knex({
   client: 'pg',
   connection: {
-  	//connectionString: 'dpg-ce12ahg2i3mkuce8j94g-a.singapore-postgres.render.com',
-  	connectionString: process.env.DATABASE_URL, 
-  	ssl: {rejectUnauthorized: false},	
-    host : process.env.DATABASE_HOST,
-    port : 5432,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PW,
-    database : process.env.DATABSE_DB
+    //connectionString: 'dpg-cgvtdeiut4mc4kalq6mg-a.singapore-postgres.render.com',
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABSE_DB
   }
 });
 
@@ -42,25 +42,25 @@ const app = express();
 
 //instead of body-parser
 //to make req.body work properly
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send('success');
+  res.send('success');
 })
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
 app.listen(PORT, () => {
-	console.log(`app is running on port ${PORT}`);
+  console.log(`app is running on port ${PORT}`);
 })
